faac9b7751f8bba946c710b974d258f7db5e1225
mjunaid6 <mohmmadjunaid007@gmail.com>
2025-11-02 01:27:21 +0530

    Added the User and other entities:

---
+++ backend/src/test/java/com/chatwave/ChatWave/ChatWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatwave.ChatWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
